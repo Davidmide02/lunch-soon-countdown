39345024b98bd5a67a05a74443c2226ecbac50d9
Davidmide Adenusi <Davidmide07@gmail.com>
2023-03-16 03:47:55 +0100

    Adjusgtment

--- time.js
+++ time.js
@@ -11,21 +11,17 @@ function getTime() {
     const divisor = etDate - currenTime;
     console.log(divisor);
 
-    const setDay =   Math.floor(divisor / days);
-    const setMin = Math.floor((divisor%days)/hours)
-    const setHour = Math.floor((divisor%hours)/minutes)
-    const setSec = Math.floor((divisor%minutes)/seconds)
-    console.log(setMin)
-    console.log(setDay)
-    console.log(setSec)
-
-
-
-    console.log(setDay)
-
-
-    console.log(etDate)
-    console.log(currenTime);
+    let setDay = Math.floor(divisor / days);
+    let setMin = Math.floor((divisor % days) / hours);
+    let setHour = Math.floor((divisor % hours) / minutes);
+    let setSec = Math.floor((divisor % minutes) / seconds);
+    // testing
+    // console.log(setMin)
+    // console.log(setDay)
+    // console.log(setSec)
+    // console.log(setDay)
+    // console.log(etDate)
+    // console.log(currenTime);
 
 
     let dayD = document.querySelector("#day-D");
@@ -33,14 +29,20 @@ function getTime() {
     let minD = document.querySelector("#min-D");
     let secD = document.querySelector("#sec-D");
 
-    dayD.innerHTML=setDay;
+
+    setDay = setDay < 10 ? "0" + setDay : setDay;
+    setHour = setHour < 10 ? "0" + setHour : setHour;
+    setMin = setMin < 10 ? "0" + setMin : setMin;
+    setSec = setSec < 10 ? "0" + setSec : setSec;
+
+    dayD.innerHTML = setDay;
     hrD.innerHTML = setHour;
     minD.innerHTML = setMin;
     secD.innerHTML = setSec
-    // setTimeout(setSec, 1000)
+
 
 
 }
 
 setInterval(getTime, 1000)
-// getTime()
+// getTime()    
